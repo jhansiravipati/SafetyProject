9b8f485058d8638110c4b18c7bf65c0c6fc31b3d
Jhansi Ravipati <jhansiravipati2001@gmail.com>
2020-08-08 13:28:15 +0530

    Connecting to Firebase (Testing Storing and Retrieving)

---
+++ FirebaseTest2/app/src/main/java/com/example/firebasetest/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.firebasetest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+public class MainActivity extends AppCompatActivity {
+    private EditText et;
+    private Button btn,btn2;
+    private  TextView tv;
+    DatabaseReference database;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et=(EditText)findViewById(R.id.et);
+        btn=(Button)findViewById(R.id.btn);
+        btn2= (Button)findViewById(R.id.btn2);
+        tv= (TextView)findViewById(R.id.tv);
+
+        database= FirebaseDatabase.getInstance().getReference().child("Info");
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String value = et.getText().toString();
+                database.child("value").setValue(value);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                database.child("value").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String value = dataSnapshot.getValue(String.class);
+                        tv.setText(value);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+
+
+    }
+}
